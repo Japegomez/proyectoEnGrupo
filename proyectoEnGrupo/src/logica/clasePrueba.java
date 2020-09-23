865d615b69d02026152a1c95f6e7a1b5a89eed95
Javier Peña <japegomez@opendeusto.es>
2020-09-23 14:55:36 +0200

    probando

--- proyectoEnGrupo/src/logica/clasePrueba.java
+++ proyectoEnGrupo/src/logica/clasePrueba.java
@@ -2,9 +2,9 @@ package logica;
 
 public class clasePrueba {
 	public static void main(String[] args) {
-		System.out.println("adios");
 		System.out.println("hola");
-		System.out.println("guay");
+		System.out.println("adios");
+	
 	}
 }
 
