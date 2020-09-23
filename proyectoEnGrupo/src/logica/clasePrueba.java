ab093b3f0f0715cefd3a91269b95e0b47dde4861
Jon Lopez <jonlopez@opendeusto.es>
2020-09-23 11:21:26 +0200

    seguimos de prueba

--- proyectoEnGrupo/src/logica/clasePrueba.java
+++ proyectoEnGrupo/src/logica/clasePrueba.java
@@ -3,17 +3,10 @@ package logica;
 public class clasePrueba {
 	public static void main(String[] args) {
 		System.out.println("hola");
-<<<<<<< HEAD
-		//comentario prueba
-		// estamos de vuelta
-		// hey
-		// carapolla
-		// hey tio
-
-=======
-		
 		System.out.println("adios");
->>>>>>> branch 'master' of https://github.com/Japegomez/proyectoEnGrupo.git
+		
+		
+		//ya me va
 	}
 }
 
