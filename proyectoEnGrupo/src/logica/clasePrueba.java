04827813c32025cccf58027b36659c6f05460ca4
Javier Peña <japegomez@opendeusto.es>
2020-09-23 13:35:48 +0200

    probando conflictos

--- proyectoEnGrupo/src/logica/clasePrueba.java
+++ proyectoEnGrupo/src/logica/clasePrueba.java
@@ -2,8 +2,8 @@ package logica;
 
 public class clasePrueba {
 	public static void main(String[] args) {
-		System.out.println("hola");
 		System.out.println("adios");
+		System.out.println("hola");
 	}
 }
 
