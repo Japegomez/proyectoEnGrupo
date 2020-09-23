97b5e6662e7314a36c722eac472baab712f55c19
Xabier Perez <xabi.pe.mo@opendeusto.es>
2020-09-23 11:06:09 +0200

    Arreglando jonlo retraso

--- proyectoEnGrupo/src/logica/clasePrueba.java
+++ proyectoEnGrupo/src/logica/clasePrueba.java
@@ -3,15 +3,12 @@ package logica;
 public class clasePrueba {
 	public static void main(String[] args) {
 		System.out.println("hola");
-<<<<<<< HEAD
+
 		//comentario prueba
 		// estamos de vuelta
 		// hey
 		// carapolla
-		// hey tio
-=======
-
->>>>>>> branch 'master' of https://github.com/Japegomez/proyectoEnGrupo.git
+		// hey tio eyyy
 	}
 }
 
