79d815599522e6d37617a4da6627a590414d1149
Jon Lopez <jonlopez@opendeusto.es>
2020-10-07 11:22:14 +0200

    atributos de los meteoritos creados

--- proyectoEnGrupo/src/logica/MeteoritoEnemigo.java
+++ proyectoEnGrupo/src/logica/MeteoritoEnemigo.java
@@ -2,4 +2,15 @@ package logica;
 
 public class MeteoritoEnemigo extends ObjetoJuego{
 
+	protected double danyoAJugador;
+
+	public double getDanyoAJugador() {
+		return danyoAJugador;
+	}
+
+	public void setDanyoAJugador(double danyoAJugador) {
+		this.danyoAJugador = danyoAJugador;
+	}
+	
+	
 }
