f6c0cd8a754bdf84e7bab49d4c50309a8a415c36
Jon Lopez <jonlopez@opendeusto.es>
2020-10-07 11:05:37 +0200

    atributos clase objetoJuego y navejugador creados

--- proyectoEnGrupo/src/logica/NaveJugador.java
+++ proyectoEnGrupo/src/logica/NaveJugador.java
@@ -1,5 +1,12 @@
 package logica;
 
 public class NaveJugador extends ObjetoJuego {
-
+	
+	protected double velocidadAtaque;
+	protected double danyoAtaque;
+	protected double ataqueCargado;
+	
+	
+	
+	
 }
--- proyectoEnGrupo/src/logica/ObjetoJuego.java
+++ proyectoEnGrupo/src/logica/ObjetoJuego.java
@@ -4,10 +4,58 @@ public class ObjetoJuego {
 	
 	protected double posX;
 	protected double posY;
-	
+	protected int vida;
+	protected double velocidadX;
+	protected double velocidadY;
+	protected double velocidadMovimiento;
 	
 	public void ObjetoJuego() {
 		
 	}
+
+	public double getPosX() {
+		return posX;
+	}
+
+	public void setPosX(double posX) {
+		this.posX = posX;
+	}
+
+	public double getPosY() {
+		return posY;
+	}
+
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
+
+	public int getVida() {
+		return vida;
+	}
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+
+	public double getVelocidadX() {
+		return velocidadX;
+	}
+
+	public void setVelocidadX(double velocidadX) {
+		this.velocidadX = velocidadX;
+	}
+
+	public double getVelocidadY() {
+		return velocidadY;
+	}
+
+	public void setVelocidadY(double velocidadY) {
+		this.velocidadY = velocidadY;
+	}
 	
+	public void mover() {
+		this.posX += this.velocidadX * this.velocidadMovimiento;
+		this.posY += this.velocidadY * this.velocidadMovimiento;
+		
+	}
 }
