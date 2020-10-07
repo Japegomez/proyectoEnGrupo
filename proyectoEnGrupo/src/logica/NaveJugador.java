bc0ed1640408a20e8c402e29c9b21ef9fffdc0ac
Jon Lopez <jonlopez@opendeusto.es>
2020-10-07 11:07:17 +0200

    getters y setters clase naveJugador

--- proyectoEnGrupo/src/logica/NaveJugador.java
+++ proyectoEnGrupo/src/logica/NaveJugador.java
@@ -6,6 +6,25 @@ public class NaveJugador extends ObjetoJuego {
 	protected double danyoAtaque;
 	protected double ataqueCargado;
 	
+	public double getVelocidadAtaque() {
+		return velocidadAtaque;
+	}
+	public void setVelocidadAtaque(double velocidadAtaque) {
+		this.velocidadAtaque = velocidadAtaque;
+	}
+	public double getDanyoAtaque() {
+		return danyoAtaque;
+	}
+	public void setDanyoAtaque(double danyoAtaque) {
+		this.danyoAtaque = danyoAtaque;
+	}
+	public double getAtaqueCargado() {
+		return ataqueCargado;
+	}
+	public void setAtaqueCargado(double ataqueCargado) {
+		this.ataqueCargado = ataqueCargado;
+	}
+	
 	
 	
 	
