c709cb27b4bf93a400d4fd9d808b7316a43757df
Xabier Perez <xabi.pe.mo@opendeusto.es>
2020-10-09 16:14:04 +0200

    Javadocs NaveJugador y ObjetoJuego

--- proyectoEnGrupo/src/logica/NaveJugador.java
+++ proyectoEnGrupo/src/logica/NaveJugador.java
@@ -1,32 +1,39 @@
 package logica;
 
 public class NaveJugador extends ObjetoJuego {
-	// esta es la nave
+	
 	protected double velocidadAtaque;
 	protected double danyoAtaque;
 	protected double ataqueCargado;
 	
 	
+	/**Devuelve la velocidad de ataque de la nave
+	 * @return Velocidad de ataque de la nave
+	 */
 	public double getVelocidadAtaque() {
 		return velocidadAtaque;
 	}
+	
+	/**Modifica la velocidad de ataque de la nave
+	 * @param velocidadAtaque Nueva velocidad de ataque de la nave
+	 */
 	public void setVelocidadAtaque(double velocidadAtaque) {
 		this.velocidadAtaque = velocidadAtaque;
 	}
+	
+	/**Devuelve el danyo del ataque de la nave
+	 * @return Danyo de ataque de la nave
+	 */
 	public double getDanyoAtaque() {
 		return danyoAtaque;
 	}
+	
+	/**Modifica el danyo de ataque de la nave
+	 * @param danyoAtaque Nuevo danyo de ataque de la nave
+	 */
 	public void setDanyoAtaque(double danyoAtaque) {
 		this.danyoAtaque = danyoAtaque;
 	}
-	public double getAtaqueCargado() {
-		return ataqueCargado;
-	}
-	public void setAtaqueCargado(double ataqueCargado) {
-		this.ataqueCargado = ataqueCargado;
-	}
-	
-	
 	
 	
 }
