6534c08c9ecee71947ebaabf609b895a4f8c8c70
Jon Lopez <jonlopez@opendeusto.es>
2020-10-09 17:02:38 +0200

    creando la ventana login

--- proyectoEnGrupo/src/gui/VentanaLogin.java
+++ proyectoEnGrupo/src/gui/VentanaLogin.java
@@ -1,6 +1,6 @@
 package gui;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class VentanaLogin extends JFrame{
 	/**
@@ -8,6 +8,25 @@ public class VentanaLogin extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	public JPanel pPrincipal;
+	
+	public VentanaLogin() {
+		
+		pPrincipal = new JPanel();
+		
+		JLabel lNombreUsuario = new JLabel("Nombre de Usuario");
+		JTextField tfNombreUsuario = new JTextField("Introduce el nombre de Usuario");
+		
+		JLabel lContrasenya = new JLabel("Contrasenya");
+		JTextField tfContrasenya = new JTextField("Introduce la Contrasenya");
+		
+		
+		
+		
+		
+		
+		
+	}
 	
 	
 }
