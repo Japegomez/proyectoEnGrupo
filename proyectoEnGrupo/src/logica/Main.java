37a3ac19400eaeefc6053dc6b56d54492e237960
Javier Peña <japegomez@opendeusto.es>
2020-10-14 18:25:09 +0200

    look and feel implementado

--- proyectoEnGrupo/src/logica/Main.java
+++ proyectoEnGrupo/src/logica/Main.java
@@ -1,9 +1,14 @@
 package logica;
 
+import javax.swing.UIManager;
+
 import gui.*;
 
 public class Main {
 public static void main(String[] args) {
+//	try { 
+//		UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+//	} catch (Exception e) { } 
 	VentanaLogin vLogin = new VentanaLogin("Login");
 	vLogin.setVisible(true);
 }
