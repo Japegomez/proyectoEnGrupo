fe98a8f0c5d0625efc00b3ee99006530138184b2
Japegomez <japegomez@opendeusto.es>
2020-10-21 17:54:37 +0200

    commit resuelto

--- src/logica/Main.java
+++ src/logica/Main.java
@@ -5,7 +5,7 @@ import javax.swing.UIManager;
 import gui.*;
 
 public class Main {
-	public static void main(String[] arg) {
+	public static void main(String[] args) {
 		try { 
 			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 		} catch (Exception e) { } 
