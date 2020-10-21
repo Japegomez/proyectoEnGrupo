a03efd577f0dbfb4cb0b4055788c03596892aa04
Japegomez <japegomez@opendeusto.es>
2020-10-21 17:50:46 +0200

    prueba

--- src/logica/Main.java
+++ src/logica/Main.java
@@ -7,6 +7,7 @@ import gui.*;
 public class Main {
 public static void main(String[] argsjon) {
 	try { 
+		System.out.println("hola");
 		UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 		System.out.print("hola");
 	} catch (Exception e) { } 
