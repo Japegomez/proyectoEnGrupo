42679d659677dbd91ea558e4e1e398b42a64cd3c
Japegomez <japegomez@opendeusto.es>
2020-10-21 17:44:13 +0200

    update javi

--- src/logica/Main.java
+++ src/logica/Main.java
@@ -8,6 +8,7 @@ public class Main {
 public static void main(String[] args) {
 	try { 
 		UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		System.out.print("hola");
 	} catch (Exception e) { } 
 	VentanaLogin vLogin = new VentanaLogin("Login");
 	vLogin.setVisible(true);
