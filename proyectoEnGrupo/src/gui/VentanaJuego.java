a3b5ad949493c1cc4895b43f5202dc4c074c9d2e
Jon Lopez <jonlopez@opendeusto.es>
2020-10-21 13:34:54 +0200

    quitar pruba de 3 meteoritos

--- proyectoEnGrupo/src/gui/VentanaJuego.java
+++ proyectoEnGrupo/src/gui/VentanaJuego.java
@@ -34,11 +34,9 @@ public class VentanaJuego extends JFrame {
 	}
 
 	public void creaMeteorito() {
-		for(int i =0 ;i <3 ;i++) {
 			MeteoritoEnemigo me1 = new MeteoritoEnemigo();
-		me1.setPosX(Math.random()*(this.getWidth()-me1.getlMeteorito().getWidth())+me1.getlMeteorito().getWidth());
-		me1.setPosY(me1.getlMeteorito().getHeight());
-		pPrincipal.add(me1.getlMeteorito());
-		}
+			me1.setPosX(Math.random()*(this.getWidth()-me1.getlMeteorito().getWidth())+me1.getlMeteorito().getWidth());
+			me1.setPosY(me1.getlMeteorito().getHeight());
+			pPrincipal.add(me1.getlMeteorito());
 	}
 }
