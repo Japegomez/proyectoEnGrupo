93f5103dfd98a7657d04d45a7b48e3af37a85f0d
Japegomez <japegomez@opendeusto.es>
2020-10-21 17:48:46 +0200

    conflicto resuelto

--- src/logica/Main.java
+++ src/logica/Main.java
@@ -5,11 +5,7 @@ import javax.swing.UIManager;
 import gui.*;
 
 public class Main {
-<<<<<<< HEAD
-public static void main(String[] argsjavi) {
-=======
 public static void main(String[] args) {
->>>>>>> 243126e634acb32675850229a820b770e0271672
 	try { 
 		UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 		System.out.print("hola");
